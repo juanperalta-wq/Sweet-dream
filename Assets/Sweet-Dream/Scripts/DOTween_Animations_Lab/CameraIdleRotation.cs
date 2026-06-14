c4647888c0cdce11db6ae67bc958465596972825
SeishiroNagi291207 <pieroac2912@gmail.com>
2026-06-13 23:48:07 -0500

    Cámara de Vigilancia con DOTween
    Se Implemento los archivos de DOTween, LaCámaradevigilancia.fbx, y los scripts para poder hacer una camará funcional y con efectos algo simples usando curvas

---
+++ Assets/Sweet-Dream/Scripts/DOTween_Animations_Lab/CameraIdleRotation.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraIdleRotation : MonoBehaviour
+{
+    [SerializeField] private float angle = 45f;
+    [SerializeField] private float moveTime = 4f;
+    [SerializeField] private float waitTime = 1.5f;
+
+    private void Start()
+    {
+        Sequence patrol = DOTween.Sequence();
+        
+        patrol.Append(transform.DOLocalRotate(new Vector3(0, angle, 0),moveTime));
+
+        patrol.AppendInterval(waitTime);
+
+        patrol.Append(transform.DOLocalRotate(new Vector3(0, -angle, 0),moveTime * 2));
+
+        patrol.AppendInterval(waitTime);
+
+        patrol.SetEase(Ease.Linear);
+        patrol.SetLoops(-1);
+    }
+}
