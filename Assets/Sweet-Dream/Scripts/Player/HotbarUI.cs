ed46fb0b43f372422bd3908a3a1499605d27d9dc
Y3st5 <juandiex2005@gmail.com>
2026-06-28 17:33:10 -0500

    particulas-Icons
    se agregaron mas particulas y se implementaron los sprites de los items

---
+++ Assets/Sweet-Dream/Scripts/Player/HotbarUI.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class HotbarUI : MonoBehaviour
+{
+    [SerializeField] private List<Image> slots;
+    [SerializeField] private Color selectedColor = Color.yellow;
+    [SerializeField] private Color defaultColor = Color.white;
+    [SerializeField] private Color emptyColor = new Color(1, 1, 1, 0);
+
+    private int currentSelected = 0;
+
+    private void OnEnable()
+    {
+        InventoryManager.OnInventoryChange += UpdateSlots;
+        InventoryManager.OnItemSelect += UpdateSelected;
+    }
+
+    private void OnDisable()
+    {
+        InventoryManager.OnInventoryChange -= UpdateSlots;
+        InventoryManager.OnItemSelect -= UpdateSelected;
+    }
+
+    private void UpdateSlots(CircularDoubleLinkedList<IInteractable> inventory)
+    {
+        // Limpiar todos los slots
+        foreach (Image slot in slots)
+        {
+            slot.sprite = null;
+            slot.color = emptyColor;
+        }
+
+        // Llenar slots con los items del inventario
+        Node<IInteractable> current = inventory.head;
+        for (int i = 0; i < inventory.Count && i < slots.Count; i++)
+        {
+            if (current?.Value is ItemPickUp item && item.ItemData.Icon != null)
+            {
+                slots[i].sprite = item.ItemData.Icon;
+                slots[i].color = i == currentSelected ? selectedColor : defaultColor;
+            }
+            current = current.Next;
+        }
+    }
+
+    private void UpdateSelected(int index)
+    {
+        currentSelected = index;
+
+        for (int i = 0; i < slots.Count; i++)
+        {
+            if (slots[i].sprite != null)
+                slots[i].color = i == index ? selectedColor : defaultColor;
+        }
+    }
+}
